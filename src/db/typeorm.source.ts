891acb6e8e686e7096538157dbad0e3926b137a7
Hikmat Rajabli <iamrajabli@outlook.com>
2023-10-13 15:37:20 +0400

    [load ConfigModule]

--- src/db/typeorm.source.ts
+++ src/db/typeorm.source.ts
@@ -1,8 +1,9 @@
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { typeOrmConfig } from '../config/typeorm.config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 export default (async function () {
+	ConfigModule.forRoot();
 	const configService = new ConfigService();
 
 	const config = await typeOrmConfig(configService);
